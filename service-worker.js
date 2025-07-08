e4de6a450c4a2b7f25c785f6f4a900beca49bd5f
RiyAndhi <128784894+RiyAndhi@users.noreply.github.com>
2025-07-08 23:43:05 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = "aquarium-v1";
+const CACHE_NAME = 'smart-aquarium-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
+  '/',
+  '/index.html',
+  '/main.html',
+  '/assets/css/style.css',
+  '/assets/css/styles.css',
+  '/assets/img/icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
